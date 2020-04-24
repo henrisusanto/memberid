7d7893578c7566fbf12f496e2c49638f60ca3679
Henri Susanto <henri@member.id>
2020-04-24 16:55:43 +0700

    client get member list

--- Core/Member/RepositoryInterface/member.repository.interface.ts
+++ Core/Member/RepositoryInterface/member.repository.interface.ts
@@ -1,6 +1,16 @@
 import { MemberEntity } from '../Entity/member.entity'
 
+interface memberListParameter {
+  limit: number,
+  offset: number,
+  search: string,
+  searchableFields: string[],
+  orderBy: string,
+  order: string
+}
+
 export interface MemberRepositoryInterface {
 	generateId (): Promise <number>;
 	save (data: MemberEntity): Promise <void>;
+	findAll (parameters: memberListParameter): Promise <MemberEntity[]>;
 }
---
+++ Core/Member/UseCase/client.getmemberlist.usecase.ts
@@ -0,0 +1,45 @@
+import { MemberRepositoryInterface } from '../RepositoryInterface/member.repository.interface'
+
+export class ClientGetMemberList {
+	protected repository
+
+	constructor (repositoryConcrete: MemberRepositoryInterface) {
+		this.repository = repositoryConcrete
+	}
+
+  public async  execute (
+    recordPerPage: number,
+    currentPage: number,
+    searchKeyword: string,
+    sortBy: string,
+    order: string
+  ) {
+
+    recordPerPage = recordPerPage || 5
+    currentPage = currentPage || 1
+    searchKeyword = searchKeyword || ''
+    sortBy = sortBy || 'id'
+    order = order && 'desc' === order.toLowerCase () ? 'DESC' : 'ASC'
+
+    try {
+      const listOfMemberEntity = 
+      await this.repository.findAll({
+        limit: recordPerPage,
+        offset: (currentPage - 1) * recordPerPage,
+        search: searchKeyword,
+        searchableFields: ['FullName', 'Email', 'PhoneNumber'],
+        orderBy: sortBy,
+        order
+      })
+
+      let listOfJSONObj = listOfMemberEntity.map(memberEntity => {
+        return memberEntity.exportToPersistence()
+      })
+
+      return listOfJSONObj
+    } catch (error) {
+      throw new Error (error)
+    }
+  }
+
+}
